d80add73d81cae3a004cc53940c9f8cadf1ba57e
Adebowale Akinola <adebowalen@yahoo.com>
2015-12-20 21:39:21 +0100

    update test

--- test/FlowMasterSpec.js
+++ test/FlowMasterSpec.js
@@ -1,86 +1,134 @@
+'use strict';
+
 // Tests the FlowMaster Module.
 var should = require('should');
 var MongoClient = require('mongodb').MongoClient;
 
-var FlowMaster = require('../FlowMaster.js');
+var FlowMaster = require('../index.js');
 var fm = new FlowMaster();
 var curTime = new Date().getTime();
 
+
 function saveTime(args, callback) {
   var db = args[0];
-  db.collection('fmTest').insert({timeNow: curTime}, function(err, result){
-    callback(err, curTime);
+  db.collection('fmTest').insert({timeNow: curTime},
+    function(err, result) {
+      if (err) {
+        callback(err, {status: 'failed', message: 'TIME_NOT_SAVED',
+          data: curTime});
+      } else {
+        callback(null, {status: 'success', message: 'TIME_SAVED',
+          data: curTime});        
+      } 
   });
+
 }
 
 function getID(args, previousResult, callback) {
   var db = args[0];
-  db.collection('fmTest').findOne({timeNow: previousResult},
-    {_id: 1}, function(err, result){
-    callback(err, result._id);
+  db.collection('fmTest').findOne({timeNow: previousResult.data},
+    {_id: 1}, function(err, result) {
+
+      if (err) {
+        callback(err, {status: 'failed', message: 'UNABLE_TO_GET_ID'});
+      } else {
+        callback(null, {status: 'success', message: 'GOT_ID',
+          data: result._id}); 
+      }
+
   });
 
 }
 
 function addFName(args, previousResult, callback) {
   var db = args[0];
-
-  db.collection('fmTest').update({_id: previousResult},
-                            {$set: {fname: args[1]}}, function(err, result){
-    callback(err, previousResult);
+  db.collection('fmTest').update({_id: previousResult.data},
+    {$set: {fname: args[1]}}, function(err, result){
+      
+      if (err) {
+        callback(err, {status: 'failed', message: 'FNAME_NOT_ADDED'});
+      } else {
+        callback(null, {status: 'success', message: 'FNAME_ADDED',
+          data: previousResult.data});   
+      }
+  
   });
 
 }
 
 function addLName(args, previousResult, callback) {
   var db = args[0];
-  db.collection('fmTest').update({_id: previousResult},
-                            {$set: {lname: args[2]}}, function(err, result){
-    callback(err, previousResult);
+  db.collection('fmTest').update({_id: previousResult.data},
+    {$set: {lname: args[2]}}, function(err, result){
+  
+      if (err) {
+        callback(err, {status: 'failed', message: 'LNAME_NOT_ADDED'});
+      } else {
+        callback(null, {status: 'success', message: 'LNAME_ADDED',
+          data: previousResult.data}); 
+      }
+
   });
 }
 
 function addGender(args, previousResult, callback) {
   var db = args[0];
-  db.collection('fmTest').update({_id: previousResult},
-                            {$set: {gender: args[3]}}, function(err, result){
-    callback(err, previousResult);
+  db.collection('fmTest').update({_id: previousResult.data},
+    {$set: {gender: args[3]}}, function(err, result){
+
+      if (err) {
+        callback(err, {status: 'failed', message: 'GENDER_NOT_ADDED'});
+      } else {
+        callback(null, {status: 'success', message: 'GENDER_ADDED',
+          data: previousResult.data}); 
+      }
+  
   });
 }
 
 function getRec(args, previousResult, callback) {
   var db = args[0];
-  db.collection('fmTest').findOne({_id: previousResult},
+  
+  db.collection('fmTest').findOne({_id: previousResult.data},
     function(err, result){
-    callback(err, result);
+    
+    if (err) {
+      callback(err, {status: 'failed', message: 'REC_NOT_GOT'});
+    } else {
+      callback(null, {status: 'success', message: 'GOT_REC',
+        data: result}); 
+    }
+    
     db.close();
   });
 
 }
 
 function parallelFunction(item, callback) {
-  console.log('Running async op, callback in 1000ms.');
-  setTimeout(function() { callback(null, item * 2); }, 1000);
+  setTimeout(function() { callback(null, item * 2); }, 20);
 }
 
 describe('FlowMaster Module', function(){
-
-  it('should run/pass series test', function(done){
+  
+  it('should pass series test', function(done){
+    
     MongoClient.connect('mongodb://localhost:27017/test',
       function(err, db) {
-      'use strict';
-      var arrayOfFunctions = [saveTime, getID, addFName, addLName,
-                                addGender, getRec];
-      fm.series(db, 'Wale', 'Nureni', 'Male', arrayOfFunctions,
-        function(err, result){
-        result.lname.should.equal('Nureni');
-        db.collection('fmTest').drop();
-        done();
-      });
+       
+        var arrayOfFunctions = [saveTime, getID, addFName, addLName,
+                                  addGender, getRec];
+        
+        fm.series(db, 'Adebowale', 'Akinola', 'Male', arrayOfFunctions,
+          function(err, result) {
+            result.data.lname.should.equal('Akinola');
+            done();
+        });
+    
     });
+  
   });
 
-  it('should run/pass parallel test', function(done){
+  it('should pass parallel test', function(done){
     var parallelItems = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
     fm.parallel(parallelItems, parallelFunction, function(err, result){
